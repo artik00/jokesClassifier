f6c13e5e6a987c00bfcc35dae89299a5392c7ddc
artik00 <art.artik@gmail.com>
2019-07-22 22:09:46 +0300

    Added Data Loader

--- DataLoader.py
+++ DataLoader.py
@@ -4,10 +4,11 @@ import git
 
 class DataLoader:
 
-    def __init__(self, url, directory_to_clone_to):
+    def __init__(self, url, directory_to_clone_to, file_to_extract = None):
         self.url_to_clone = url
         self.dir = directory_to_clone_to
         self.copy_data_set()
+
 
 
     def copy_data_set(self):
--- test_data_loader.py
+++ test_data_loader.py
@@ -4,5 +4,5 @@ import os
 class TestDataLoader:
 
     def test_data_loader(self):
-        dataloader = DataLoader('https://github.com/taivop/joke-dataset.git', 'datasets')
-        assert os.path.exists('datasets')
+        dataloader = DataLoader('https://github.com/taivop/joke-dataset.git', 'datasets/jokes')
+        assert os.path.exists('datasets/jokes')
