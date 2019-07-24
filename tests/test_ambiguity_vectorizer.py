c1608de3a066497ddc8d9207b8fc056f4fe23995
artik00 <art.artik@gmail.com>
2019-07-24 22:28:00 +0300

    Added AmbiguityVectorizer

---
+++ tests/test_ambiguity_vectorizer.py
@@ -0,0 +1,17 @@
+from AmbiguityVectorizer import get_post_tags_from_nltk
+from utils.wordnet_utils import get_similar_words_from_wordnet
+
+class TestAmbiguityVectorizer:
+
+    def test_pos_tagger(self):
+        sentences = ['Jesus loves you but everyone else thinks you\'re an asshole'.split(),'A clear conscience is usually the sign of a bad memory'.split()]
+
+        pos_sentences = get_post_tags_from_nltk(sentences)
+        assert len(pos_sentences) == 2
+        assert len(pos_sentences[0]) == 10
+        assert pos_sentences[0][0][1] == 'NNP'
+
+
+    def test_similarity_path(self):
+        similar_words = get_similar_words_from_wordnet('world', 'NN')
+        print(similar_words)
