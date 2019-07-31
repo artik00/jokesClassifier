eeaa4895fe7be719d69e700199c39b76c2c78b15
amirl <amir4895@gmail.com>
2019-07-31 23:01:36 +0300

    infrastructure and phonetic class

---
+++ w2v_vectorizer.py
@@ -0,0 +1,29 @@
+import numpy as np
+import gensim
+
+
+class W2vVectorizer:
+    
+    def __init__(self, model: gensim.models, sentences: list):
+        """
+        
+        :param model: w2v  model function  
+        :param sentences: sentences to be vectorize in list
+        """
+        self.model = model
+        self.sentences = sentences
+        
+    def vectorizer(self):
+        feature_w2v = []
+        for sentence in self.sentences:
+            sum_vector = np.zeros(self.model.vector_size)
+            for word in sentence:
+                try:
+                    score = self.model.get_vector(word)
+                except KeyError as e:
+                    score = np.zeros(self.model.vector_size)
+                sum_vector = sum_vector + score
+            avg_vector = sum_vector / len(sentence) if sentence else np.zeros(self.model.vector_size)
+            feature_w2v.append(avg_vector)
+        return np.vstack(feature_w2v)
+
