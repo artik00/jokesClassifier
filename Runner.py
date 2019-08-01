e7992635a4a35c645a49e19355b44e3ba3ce287f
amir4859 <amir4895@gmail.com>
2019-08-01 23:06:58 +0300

    infrastructure and phonetic class (#10)

--- Runner.py
+++ Runner.py
@@ -5,6 +5,7 @@ from data_loader_from_file import DataLoaderFromFile
 from interpersonal_vectorizer import InterpersonalVectorizer
 from incogurity_vectorizer import IncogurityVectorizer
 from phonetic_style import PhoneticStyle
+from w2v_vectorizer import W2vVectorizer
 import numpy as np
 import gensim
 
@@ -16,8 +17,11 @@ if __name__ == "__main__":
     jokes_file = 'dataset/Jokes16000.txt'
     model = gensim.models.KeyedVectors.load_word2vec_format(word2vec_pretrained,
                                                             binary=True, limit=50000)
+
     NUMBER_OF_SENTENCES = 10
     dataloader = DataLoaderFromFile(jokes_file, max_number_of_sentences=NUMBER_OF_SENTENCES)
+    word_vectorizer = W2vVectorizer(model=model, sentences=dataloader.get_all_sentences_splitted_by_word())
+    word2vec_vector = word_vectorizer.vectorizer()
     phonetic_vector = PhoneticStyle(dataloader.get_all_sentences()).vector
     jokes_splitted_by_word = dataloader.get_all_sentences_splitted_by_word()
 
@@ -27,6 +31,6 @@ if __name__ == "__main__":
     interpersonal_vectorizer = InterpersonalVectorizer()
     interpersonal_vector = interpersonal_vectorizer.get_feature_vector(sentences=jokes_splitted_by_word)
     full_vector = list()
-    temp = np.concatenate((phonetic_vector, ambiguity_vector, incogurity_vector, interpersonal_vector), axis=1)
+    temp = np.concatenate((phonetic_vector, ambiguity_vector, incogurity_vector, interpersonal_vector, word2vec_vector), axis=1)
     pass
 
