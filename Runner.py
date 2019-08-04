80f2d503617c4a929124c58acb8d47cf5f674d6e
amirl <amir4895@gmail.com>
2019-08-04 22:31:10 +0300

    adding cnn -  new

--- Runner.py
+++ Runner.py
@@ -1,7 +1,11 @@
 import gensim
 from learning_pipe import LearningPipe
+from cnn.cnn import CNN
+import numpy as np
+
 word2vec_pretrained = 'word2vec/GoogleNews-vectors-negative300.bin.gz'
 SHORT_RUN = True
+NUMBER_OF_SENTENCES = 10
 
 
 if __name__ == "__main__":
@@ -9,13 +13,24 @@ if __name__ == "__main__":
     non_jokes_file = 'dataset/MIX16000.txt'
     model = gensim.models.KeyedVectors.load_word2vec_format(word2vec_pretrained,
                                                             binary=True, limit=50000)
-    NUMBER_OF_SENTENCES = 20
+
+
     jokes_total_data_instnce = LearningPipe(data_path=jokes_file,  w2v_model=model,
                                             max_number_of_sentences=NUMBER_OF_SENTENCES)
     non_jokes_data_instance = LearningPipe(data_path=non_jokes_file,  w2v_model=model,
                                            max_number_of_sentences=NUMBER_OF_SENTENCES)
     jokes_total_vector = jokes_total_data_instnce.get_vectorize_data()
     non_jokes_total_vector = non_jokes_data_instance.get_vectorize_data()
+
+
+    labels = [0] * NUMBER_OF_SENTENCES + [1] * NUMBER_OF_SENTENCES
+    labels = np.asarray(labels)
+    concatenated_data = np.concatenate([jokes_total_vector, non_jokes_total_vector])
+
+    cnn_instance = CNN(data=concatenated_data, labels=labels)
+    cnn_model = cnn_instance.model
+
+
 
     pass
 
---
+++ cnn/cnn.py
@@ -0,0 +1,67 @@
+import keras
+from keras.layers import Input, Dense
+from keras.models import Model
+import numpy as np
+import pickle
+
+
+VALIDATION_SPLIT = 0.20
+
+
+class CNN:
+
+    def __init__(self, data: np.ndarray, labels):
+        self.model = None
+        data_to_3d = data.reshape(-1, data.shape[0], data.shape[1] )
+        indices = np.arange(data.shape[0])
+        np.random.shuffle(indices)
+        data = data[indices]
+        labels = labels[indices]
+        nb_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
+
+        x_train = data[:-nb_validation_samples]
+        y_train = labels[:-nb_validation_samples]
+        x_test = data[-nb_validation_samples:]
+        y_test = labels[-nb_validation_samples:]
+
+        batch_size = 10
+        epochs = 1
+        input_vec = Input(shape=(data.shape[1],))
+        dense_0 = Dense(20, activation='relu')(input_vec)
+        dense_1 = Dense(10, activation='relu')(dense_0)
+        dense_2 = Dense(1, activation='sigmoid')(dense_1)
+        model = Model(inputs=input_vec, outputs=dense_2)
+        model.summary()
+        # model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+
+
+        # compile the model
+        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc',
+                                                     self.f1_m, self.precision_m, self.recall_m])
+        history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1)
+
+        loss, accuracy, f1_score, precision, recall = model.evaluate(x_test, y_test, verbose=0)
+
+        self.model = model
+        print(f"The testing values are: \n Loss: {loss}\n Accuracy: {accuracy}\n F1_score: {f1_score}\n "
+              f"Percision: {precision}\n Recall {recall}")
+        pickle.dump(model, open("/model/model.sav", 'wb'))
+
+    def recall_m(self, y_true, y_pred):
+        true_positives = keras.backend.sum(keras.backend.round(keras.backend.clip(y_true * y_pred, 0, 1)))
+        possible_positives = keras.backend.sum(keras.backend.round(keras.backend.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + keras.backend.epsilon())
+        return recall
+
+    def precision_m(self, y_true, y_pred):
+        true_positives = keras.backend.sum(keras.backend.round(keras.backend.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = keras.backend.sum(keras.backend.round(keras.backend.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + keras.backend.epsilon())
+        return precision
+
+    def f1_m(self, y_true, y_pred):
+        precision = self.precision_m(y_true, y_pred)
+        recall = self.recall_m(y_true, y_pred)
+        return 2 * ((precision * recall) / (precision + recall + keras.backend.epsilon()))
+
