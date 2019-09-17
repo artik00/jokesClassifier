8b334e3037ed14d1c1eb78e9b6750c08e7db51f1
amirl <amir4895@gmail.com>
2019-09-17 22:05:21 +0300

    adding cnn -  adding load model

--- Runner.py
+++ Runner.py
@@ -6,18 +6,18 @@ from cnn.cnn_model_evaluator import CnnEvaluator
 from data_loader_from_file import DataLoaderFromFile
 
 SHORT_RUN = True
-NUMBER_OF_SENTENCES = 10
+NUMBER_OF_SENTENCES = 16000
 
 
 if __name__ == "__main__":
     jokes_file = 'dataset/Jokes16000.txt'
     non_jokes_file = 'dataset/MIX16000.txt'
 
-    eval_pipe = EvaluationPipe('dataset/sentences_for_anchoring.txt', 10, 'model/model.sav',
-                               'dataset/amir_anchors.txt', 'dataset/artyom_anchors.txt')
-    eval_pipe.evaluate()
+    # eval_pipe = EvaluationPipe('dataset/sentences_for_anchoring.txt', 10, 'model/model.sav',
+    #                            'dataset/amir_anchors.txt', 'dataset/artyom_anchors.txt')
+    # eval_pipe.evaluate()
 
-    eval_pipe.print_anchoring_accuracy()
+    # eval_pipe.print_anchoring_accuracy()
 
     jokes_total_data_instnce = LearningPipe(data_path=jokes_file,
                                             max_number_of_sentences=NUMBER_OF_SENTENCES)
@@ -34,11 +34,11 @@ if __name__ == "__main__":
     cnn_instance = CNN(data=concatenated_data, labels=labels)
     cnn_model = cnn_instance.model
 
-    #cnn_eval = CnnEvaluator('model/model.sav', 'dataset/sentences_for_anchoring.txt')
-
-    #data_loader = DataLoaderFromFile('dataset/sentences_for_anchoring.txt', 10)
-
-    #anchoring = LearningPipe(data_path='dataset/sentences_for_anchoring.txt',  w2v_model=model,
+    # cnn_eval = CnnEvaluator('model/model.sav', 'dataset/sentences_for_anchoring.txt')
+    #
+    # data_loader = DataLoaderFromFile('dataset/sentences_for_anchoring.txt', 10)
+    #
+    # anchoring = LearningPipe(data_path='dataset/sentences_for_anchoring.txt',  w2v_model=model,
     #                                        max_number_of_sentences=1)
 
     #sentences_to_evaluate = anchoring.get_vectorize_data()
